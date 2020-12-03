97f9cdbe19f7b6ccb6cbca3fa6757089b11332f5
alexshaulskyi <alexshaulskyi@gmail.com>
2020-12-03 15:06:59 +0300

    removed redundant import

--- users/signals.py
+++ users/signals.py
@@ -7,7 +7,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db.models import Avg
 
-from users.models import User, Notification
+from users.models import User
 from tests.models import UserPassedTest
 
 
